7a13dacab74bf3908ff62327acf89988094bbbf8
Yash Sahni <67969545+yash-sahni54321@users.noreply.github.com>
2023-07-17 04:59:05 +0530

    UI improved map added

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -3,7 +3,7 @@ import { useHistory } from "react-router";
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="max-w-sm p-6  border  rounded-lg shadow bg-gray-900 border-gray-700 hover:shadow-lg">
+    <div className="max-w-sm p-6  border  rounded-lg shadow bg-gray-50 border-gray-700 hover:shadow-lg">
       {/* <div className="items-center h-3/4 flex justify-center">
                 <img className="object-contain" src={product.productImg} alt="Product Image"></img>
             </div> */}
@@ -16,7 +16,7 @@ const ProductCard = ({ product }) => {
         ></img>
       </div>
 
-      <div className="px-3 py-3 font-bold flex justify-center items-center text-white">
+      <div className="px-3 py-3 font-bold flex justify-center items-center text-gray-900">
         {product.name}
       </div>
       <span className="flex-wrap max-sm:flex w-max max-sm:justify-center px-3 py-3 font-bold flex justify-center items-center text-sm  bg-red-600 hover:bg-red-800 rounded-lg text-white hover:cursor-pointer">
