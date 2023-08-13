930fb56962363d884949a375dae48f9b9af371a0
Yash Sahni <67969545+yash-sahni54321@users.noreply.github.com>
2023-08-13 15:31:07 +0530

    whatsApp added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import ProductDetail from "./Pages/ProductDetail";
 import GetCallPage from "./Pages/GetCallPage";
 import Dashboard from "./Pages/DashBoard/pages/Dashboard";
 import AdminLogin from "./Pages/AdminLogin";
-import { BsFillTelephoneOutboundFill } from "react-icons/bs";
+import { BsFillTelephoneOutboundFill, BsWhatsapp } from "react-icons/bs";
 
 function App() {
   return (
@@ -65,6 +65,18 @@ function App() {
           class="fixed z-90 bottom-10 right-8 bg-red-600 w-14 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-red-700 hover:drop-shadow-2xl hover:animate-bounce duration-300 "
         >
           <BsFillTelephoneOutboundFill size={20} />
+        </button>
+      </a>
+      <a
+        href="https://wa.me/918840007880?text=Hi%20There!"
+        class="float"
+        target="_blank"
+      >
+        <button
+          title="Contact Sale"
+          class="fixed z-90 bottom-28 right-8 bg-red-600 w-14 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-red-700 hover:drop-shadow-2xl hover:animate-bounce duration-300 "
+        >
+          <BsWhatsapp size={20} />
         </button>
       </a>
     </Router>
